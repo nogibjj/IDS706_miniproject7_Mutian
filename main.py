5222f84df64b3f4998c0c3540f9d0d4debdeac51
Liam <ml646@duke.edu>
2023-10-11 17:19:11 -0400

    updata click

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 from mylib.dbfunc import connectDB, createDB, insertDB, readDB, updateDB, deleteDB
 import os
+import click
 
 
-if os.path.exists("mydb.db"):
-    os.remove("mydb.db")
-conn, cursor = connectDB("mydb.db")
+@click.command()
+@click.argument("db")
+def main(db):
 
-# C
-createDB(cursor)
-insertDB(cursor, 1, "Ethan", "Smith", 20, 1)
-insertDB(cursor, 2, "Emma", "Brown", 21, 2)
-insertDB(cursor, 3, "Lucas", "Davis", 18, 3)
-# R
-info = readDB(cursor)
+    if os.path.exists(db):
+        os.remove(db)
+    conn, cursor = connectDB(db)
+    # C
+    createDB(cursor)
+    insertDB(cursor, 1, "Ethan", "Smith", 20, 1)
+    insertDB(cursor, 2, "Emma", "Brown", 21, 2)
+    insertDB(cursor, 3, "Lucas", "Davis", 18, 3)
+    # R
+    info = readDB(cursor)
 
-print(info)
+    click.echo(info)
 
-# U
-updateDB(cursor, 1, "Ethan", "James", 23, 5)
-info = readDB(cursor)
-print(info)
+    # U
+    updateDB(cursor, 1, "Ethan", "James", 23, 5)
+    info = readDB(cursor)
+    click.echo(info)
+
+    # D
+    deleteDB(cursor, 2)
+    info = readDB(cursor)
+    click.echo(info)
+
+    conn.commit()
+    conn.close()
 
 
-# D
-deleteDB(cursor, 2)
-info = readDB(cursor)
-print(info)
-
-
-conn.commit()
-conn.close()
+if __name__ == "__main__":
+    main()
