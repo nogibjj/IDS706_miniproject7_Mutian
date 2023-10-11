69e969d39c4d737219fbb21fccb49be90348ff31
Liam <ml646@duke.edu>
2023-10-11 17:23:46 -0400

    updata click echo

--- main.py
+++ main.py
@@ -11,23 +11,31 @@ def main(db):
         os.remove(db)
     conn, cursor = connectDB(db)
     # C
+    click.echo("Connection success")
     createDB(cursor)
+    click.echo("Inserting data...")
     insertDB(cursor, 1, "Ethan", "Smith", 20, 1)
     insertDB(cursor, 2, "Emma", "Brown", 21, 2)
     insertDB(cursor, 3, "Lucas", "Davis", 18, 3)
     # R
     info = readDB(cursor)
-
+    click.echo("Read current table:")
     click.echo(info)
 
     # U
+    click.echo("\n")
+    click.echo("Updata data1")
     updateDB(cursor, 1, "Ethan", "James", 23, 5)
     info = readDB(cursor)
+    click.echo("Read current table:")
     click.echo(info)
 
     # D
+    click.echo("\n")
+    click.echo("Delete data2:")
     deleteDB(cursor, 2)
     info = readDB(cursor)
+    click.echo("Read current table:")
     click.echo(info)
 
     conn.commit()
